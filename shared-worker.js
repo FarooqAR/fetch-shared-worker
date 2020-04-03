aef8f1b3150702570a30fe905875bbd28e02ad4e
FarooqAR <farooqbckk@gmail.com>
2020-04-03 14:05:46 +0500

    add click

--- shared-worker.js
+++ shared-worker.js
@@ -1,8 +1,11 @@
 onconnect = function(e) {
     var port = e.ports[0];
-    fetch("result-from-fetch.json")
-    .then(r => r.json())
-    .then(data => {
-        port.postMessage(data);
-    });
+
+    port.onmessage = function (e) {
+        fetch("result-from-fetch.json")
+        .then(r => r.json())
+        .then(data => {
+            port.postMessage(data);
+        });
+    }
   };
